ae731218b655e29b5c746d2adc90780db66b2025
hajinopro <hajinopro@gmail.com>
2022-04-08 20:44:50 +0900

    Initial Commit

---
+++ RWFreeView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RWFreeView
+//
+//  Created by 하진호 on 2022/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RWFreeView/RWFreeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWFreeViewApp.swift
+//  RWFreeView
+//
+//  Created by 하진호 on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct RWFreeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
